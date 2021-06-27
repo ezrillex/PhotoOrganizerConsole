c819b7bdf548a76e6c01f87a4757e3318576ac6f
Ezra Abarca <ezra.alejandro.abarca.cordova@gmail.com>
2021-06-27 14:54:57 -0600

    Remove unused imports. Optimize indexing. Fix bug in directory creation.  Fix crash on duplicate file. Add handling of duplicate files. Add an optional clean up script to clear emtpy directories on target.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing;
-using System.Text.RegularExpressions;
-using System.Drawing.Imaging;
 using ShellProgressBar;
 
 namespace PhotoOrganizer
@@ -25,8 +19,8 @@ namespace PhotoOrganizer
 
         static void Main(string[] args)
         {
-
             // TODO detect if file is onedrive, on demand specefically the not synced one.
+
 
             Dictionary<int, string> Months = new Dictionary<int, string>() {
                 {1,"1 - Enero" } ,
@@ -43,15 +37,19 @@ namespace PhotoOrganizer
                 {12,"12 - Diciembre" }
             }; // Could expand by using CultureInfo to generate this data.
 
-            DirectoryInfo media_dir = new DirectoryInfo(@"A:\OneDrive\Imágenes\Álbum de cámara\2010");
+            DirectoryInfo media_dir = new DirectoryInfo(@"A:\OneDrive\Imágenes\Álbum de cámara\Desorden");
+            string output_dir = @"A:\OneDrive\Imágenes\Álbum de cámara\Orden";
+
+
+         
+
             FileInfo[] media = media_dir.GetFiles("*.*", SearchOption.AllDirectories);
 
-            string output_dir = @"A:\ImageOrganizationOutputTest\";
 
             // You could take the pedantic approach of reading metadata, and trying to get the date taken instad of last write, but tbh I dont edit past photos, 
             // and if I do I make a copy first, if that's your case (mismatching date taken metadata and last write) then go nuts with that.
 
-            List<Media> DatedMedia = new List<Media>();
+            Media[] DatedMedia = new Media[media.Length];
 
             int lowest_year = 9999;
             int highest_year = 0;
@@ -59,30 +57,40 @@ namespace PhotoOrganizer
             // Index media files and date them.
             using (var pbar = new ProgressBar(media.Length, "Indexing Files...", ProgressOptions))
             {
-                foreach (var item in media)
+                DateTime dating_of_media = new DateTime(); // reuse this to reduce stress of GC
+                for (int i = 0; i < media.Length; i++)
                 {
-                    Log($"Indexing {item.FullName}");
-                    pbar.Tick($"Indexing {item.FullName}");
+                    Log($"Indexing {media[i].FullName}"); 
+                    pbar.Tick($"Indexing {media[i].FullName}");
 
-                    DateTime dating_of_media = new DateTime();
 
                     try
                     {
-                        dating_of_media = StackOverflow.GetDateTakenFromImage(item.FullName);
+                        dating_of_media = StackOverflow.GetDateTakenFromImage(media[i].FullName);
                     }
-                    catch (ArgumentException) // also handles the exception that happens when creating an image object but its provided a video file path
+                    catch (Exception ex)
                     {
-                        dating_of_media = item.LastWriteTime;
+                        // Argument also handles the exception that happens when creating an image object but its provided a video file path
+                        if (ex is ArgumentException || ex is FormatException)
+                        {
+                            dating_of_media = media[i].LastWriteTime;
+                            if (ex is FormatException)
+                            {
+                                Log($"This file caused a FormatException: {media[i].FullName}");
+                            }
+                        }
+                        else
+                        {
+                            throw;
+                        }
                     }
 
 
-                    DatedMedia.Add(new Media(dating_of_media, item));
+                    DatedMedia[i] = new Media(dating_of_media, media[i]);
 
                     // Update lowest and highest year found to aid in folder tree generation.
                     if (dating_of_media.Year < lowest_year) lowest_year = dating_of_media.Year;
                     if (dating_of_media.Year > highest_year) highest_year = dating_of_media.Year;
-
-
 
                 }
             }
@@ -103,7 +111,8 @@ namespace PhotoOrganizer
                         Directory.CreateDirectory(output_dir + i); // Create year folders
                         foreach (KeyValuePair<int, string> mkvp in Months)
                         {
-                            string dir_path = output_dir + i + @"\" + mkvp.Value; // TODO use path.combine bruh
+                            //string dir_path = output_dir + i + @"\" + mkvp.Value; // TODO use path.combine bruh // IT ACTUALLY FUCKIN NEEDED IT BECAUSE NO SLASH BETWEEN OUT + I
+                            string dir_path = Path.Combine(output_dir, i.ToString(), mkvp.Value);
                             Log($"Creating directory {dir_path}");
                             pbar.Tick($"Creating directory {dir_path} ...");
                             Directory.CreateDirectory(dir_path); // Create month's subdirectories.
@@ -115,29 +124,50 @@ namespace PhotoOrganizer
             
 
             // Moving media files to new directory
-            Media[] DatedMedia_optimized = DatedMedia.ToArray();
-            
-            using (var pbar = new ProgressBar(DatedMedia_optimized.Length, "Moving Files...", ProgressOptions))
+            using (var pbar = new ProgressBar(DatedMedia.Length, "Moving Files...", ProgressOptions))
             {
-                for (int i = 0; i < DatedMedia_optimized.Length; i++)
+                for (int i = 0; i < DatedMedia.Length; i++)
                 {
                     string final_path = Path.Combine(
                         output_dir,
-                        DatedMedia_optimized[i].Dating.Year.ToString(),
-                        Months[DatedMedia_optimized[i].Dating.Month],
-                        DatedMedia_optimized[i].File.Name
+                        DatedMedia[i].Dating.Year.ToString(),
+                        Months[DatedMedia[i].Dating.Month],
+                        DatedMedia[i].File.Name
                         );
 
-                    Log($"Moving {DatedMedia_optimized[i].File.FullName} to {final_path}");
-                    pbar.Tick($"Moving {DatedMedia_optimized[i].File.Name} ...");
-                    //DatedMedia_optimized[i].File.CopyTo(final_path);
+                    Log($"Moving {DatedMedia[i].File.FullName} to {final_path}");
+                    pbar.Tick($"Moving {DatedMedia[i].File.Name} ...");
+                    try
+                    { 
+                        DatedMedia[i].File.MoveTo(final_path);
+                    }
+                    catch (IOException)
+                    {
+                        // TODO Log exceptions.
+                        // Handle duplicates
+                        if (File.Exists(final_path))
+                        {
+                            Log($"FILE ALREADY EXISTS EXCEPTION.");
+                            Random rng = new Random();
+                            string NewPath = Path.Combine(
+                                output_dir,
+                                DatedMedia[i].Dating.Year.ToString(),
+                                Months[DatedMedia[i].Dating.Month],
+                                (rng.Next(0,1000000).ToString() + DatedMedia[i].File.Name)
+                            );
+                            Log($"New file name: {NewPath}");
+                            DatedMedia[i].File.MoveTo(NewPath);
+                        }
+                        else
+                        {
+                            // Not a dupe, no handling available.
+                            throw;
+                        }
+                    }
                 }
             }
 
-
-           
-           
-
+            FindEmptyDirectories(new DirectoryInfo(output_dir));
 
 
             Console.WriteLine("Press any key to exit!");
@@ -160,6 +190,46 @@ namespace PhotoOrganizer
         private static void Log(object text)
         {
             File.AppendAllText($"{application_start_time}_PhotoOrganizerLog.txt", $"\n[{DateTime.Now}]\t" + text);
+        }
+
+        private static void FindEmptyDirectories(DirectoryInfo RootDirectory)
+        {
+            List<DirectoryInfo> empty_dirs = new List<DirectoryInfo>();
+
+            var dirs = RootDirectory.GetDirectories("*",SearchOption.TopDirectoryOnly);
+            foreach(var dir in dirs)
+            {
+                var files = dir.GetFiles("*.*",SearchOption.AllDirectories);
+                long folder_size = 0;
+                foreach(FileInfo file in files)
+                {
+                    folder_size += file.Length;
+                }
+                if(folder_size == 0)
+                {
+                    Console.WriteLine($"Directory is empty: {dir.FullName}");
+                    empty_dirs.Add(dir);
+                }
+
+            }
+
+            Console.WriteLine("Delete empty directories? (yes/no)");
+            string answer = Console.ReadLine();
+            if(answer == "yes")
+            {
+                foreach(var dir in empty_dirs)
+                {
+                    var sub_dirs = dir.GetDirectories();
+
+                    foreach(var sub in sub_dirs)
+                    {
+                        sub.Delete();
+                    }
+
+                    dir.Delete();
+                }
+                Console.WriteLine("Deletion Finished");
+            }
         }
     }
 }
